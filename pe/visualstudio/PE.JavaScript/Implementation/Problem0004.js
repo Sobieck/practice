ae092187b715395b4487c25f3034d26769ce0169
Sobieck00 <thomas@sobieck.com>
2015-01-02 18:54:55 -0600

    Finished Problem 4.

---
+++ pe/visualstudio/PE.JavaScript/Implementation/Problem0004.js
@@ -0,0 +1,52 @@
+﻿var javascriptPE;
+(function (javascriptPE) {
+    (function (implementation) {
+
+        var problem0004 = (function() {
+
+            function problem0004() {
+                
+            }
+
+            problem0004.prototype.isPalendrom = function(n) {
+                var nString = n.toString();
+
+                var stringLength = nString.length;
+                var index = 0;
+
+                var isPalendrom = true;
+
+                while (isPalendrom && (stringLength - (index * 2) > 1)) {
+                    if (nString[index] != nString[stringLength - (index + 1)]) {
+                        isPalendrom = false;
+                    }
+
+                    index++;
+                }
+
+                return isPalendrom;
+            };
+
+            problem0004.prototype.maxPalendromProduct = function(start, end) {
+                var maxPalendromProduct = 0;
+
+                for (var i = start; i < end + 1; i++) {
+                    for (var j = i; j < end + 1; j++) {
+                        var product = j * i;
+                        if (product > maxPalendromProduct && this.isPalendrom(product)) {
+                            maxPalendromProduct = product;
+                        }
+                    }
+                }
+
+                return maxPalendromProduct;
+            };
+            
+
+
+            return problem0004;
+        })();
+        implementation.problem0004 = problem0004;
+
+    })(javascriptPE.implementation || (javascriptPE.implementation = {}));
+})(javascriptPE || (javascriptPE = {}));
---
+++ pe/visualstudio/PE.JavaScript/Spec/Problem0004.Spec.js
@@ -0,0 +1,58 @@
+﻿var javascriptPE;
+(function (javascriptPE) {
+    (function (spec) {
+
+        (function (problem0004) {
+
+            describe('problem0004', function () {
+                var problem0004;
+                
+                beforeEach(function () {
+                    problem0004 = new javascriptPE.implementation.problem0004();
+                });
+
+                describe('isPalendrom', function() {
+                    it('should return false when input is 1231.', function() {
+                        isPalendromAssert(1231, false);
+                    });
+
+                    it('should return true when input is 1.', function () {
+                        isPalendromAssert(1, true);
+                    });
+
+                    it('should return true when input is 12221.', function () {
+                        isPalendromAssert(12221, true);
+                    });
+
+                    function isPalendromAssert(n, expected) {
+                        expect(problem0004.isPalendrom(n)).toBe(expected);
+                    };
+                });
+
+                describe('maxPalendromProduct', function() {
+                    it('should return 9 when the inputs are 1 and 9.', function() {
+                        maxPalendromProductAssert(1, 9, 9);
+                    });
+
+                    it('should return 9 when the inputs are 1 and 11.', function () {
+                        maxPalendromProductAssert(1, 11, 121);
+                    });
+
+                    it('should return 9 when the inputs are 1 and 11.', function () {
+                        maxPalendromProductAssert(10, 99, 9009);
+                    });
+
+                    it('should return 9 when the inputs are 1 and 11.', function () {
+                        maxPalendromProductAssert(100, 999, 906609);
+                    });
+
+                    function maxPalendromProductAssert(start, end, expected) {
+                        expect(problem0004.maxPalendromProduct(start, end)).toBe(expected);
+                    }
+                });
+
+            });
+
+        })(spec.problem0004 || (spec.problem0004 = {}));
+    })(javascriptPE.spec || (javascriptPE.spec = {}));
+})(javascriptPE || (javascriptPE = {}));
