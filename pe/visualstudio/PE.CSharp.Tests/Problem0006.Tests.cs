a560abbab9ea14d4242373b235b97eb1654b9981
Sobieck00 <thomas@sobieck.com>
2015-01-03 19:13:22 -0600

    C# problem 0006

---
+++ pe/visualstudio/PE.CSharp.Tests/Problem0006.Tests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PE.CSharp.Tests
+{
+    [TestClass]
+    public class Problem0006Tests
+    {
+        private static Problem0006 _problem0006;
+        
+        [ClassInitialize]
+        public static void InitializeClass(TestContext ctx)
+        {
+            _problem0006 = new Problem0006();
+        }
+
+        [TestMethod]
+        public void Problem0006_SumOfSquares_2_Should_Return_5S()
+        {
+            Assert.AreEqual(5, _problem0006.SumOfSquares(2));
+        }
+
+        [TestMethod]
+        public void Problem0006_SumOfSquares_10_Should_Return_385()
+        {
+            Assert.AreEqual(385, _problem0006.SumOfSquares(10));
+        }
+
+        [TestMethod]
+        public void Problem0006_SquareOfSums_2_Should_Return_9()
+        {
+            Assert.AreEqual(9, _problem0006.SquareOfSums(2));
+        }
+
+        [TestMethod]
+        public void Problem0006_SquareOfSums_10_Should_Return_3025()
+        {
+            Assert.AreEqual(3025, _problem0006.SquareOfSums(10));
+        }
+
+        [TestMethod]
+        public void Problem0006_DifferenceBetweenSquareOfSumsAndSumOfSquares_2_Should_Return_4()
+        {
+            Assert.AreEqual(4, _problem0006.DifferenceBetweenSquareOfSumsAndSumOfSquares(2));
+        }
+
+        [TestMethod]
+        public void Problem0006_DifferenceBetweenSquareOfSumsAndSumOfSquares_10_Should_Return_2640()
+        {
+            Assert.AreEqual(2640, _problem0006.DifferenceBetweenSquareOfSumsAndSumOfSquares(10));
+        }
+
+        [TestMethod]
+        public void Problem0006_Answer()
+        {
+            Assert.AreEqual(25164150, _problem0006.DifferenceBetweenSquareOfSumsAndSumOfSquares(100));
+        }
+    }
+}
