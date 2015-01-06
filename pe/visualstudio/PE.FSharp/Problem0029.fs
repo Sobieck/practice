d8969b831d73e822a735ce6803e370473e7b0df6
Sobieck00 <thomas@sobieck.com>
2015-01-06 12:39:30 -0600

    Finished 29 in F#.

--- pe/visualstudio/PE.FSharp/Problem0029.fs
+++ pe/visualstudio/PE.FSharp/Problem0029.fs
@@ -10,5 +10,15 @@ module Problem0029 =
         |> List.tail
         |> List.map (fun elem -> pown number elem)
 
+    let distinctPowersBetween2And number = 
+        Seq.init number (fun elem -> elem + 1)
+        |> Seq.toList
+        |> List.tail
+        |> List.map (fun elem -> BigInteger(elem))
+        |> List.map (fun elem -> exponentiateNumberFromTwoToX elem number)
+        |> List.map (fun elem -> Set.ofList elem)
+        |> Set.unionMany
+        |> Set.count
+
 
     
