fc2c7cb7701231c748c7313297332a848528a04b
Sobieck00 <thomas@sobieck.com>
2015-01-07 08:08:15 -0600

    The karma runner stopped for some reason. Fixed it.

--- KarmaRunner.bat
+++ KarmaRunner.bat
@@ -1,1 +1,1 @@
-karma start C:\Users\Thomas\Documents\GitHub\practice\karma.conf.js
+node C:\Users\Thomas\AppData\Roaming\npm\node_modules\karma-cli\bin\karma start C:\Users\Thomas\Documents\GitHub\practice\karma.conf.js
