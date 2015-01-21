4b7d02041406e53a69d8c737cb40310a03dadeda
Sobieck00 <thomas@sobieck.com>
2015-01-21 16:42:54 -0600

    Did that fix the tab issue?

--- pe/nonvisualstudio/haskell/Implementation/Problem0004.hs
+++ pe/nonvisualstudio/haskell/Implementation/Problem0004.hs
@@ -3,7 +3,7 @@ module Problem0004 where
 	zipNumberWithReverse number = zip (show number) (reverse (show number))
 
 	lengthOfArrayThatAddsOneToOddNumbers array 	| odd (length array) 	= (length array) + 1
-																							| otherwise 					= length array
+												| otherwise				= length array
 
 	halveArray array = take (n `div` 2) array
 		where n = lengthOfArrayThatAddsOneToOddNumbers array
@@ -14,8 +14,8 @@ module Problem0004 where
 
 	maxPalindromProduct smallestNumber largestNumber =
 		maximum [x * y | x <- [smallestNumber..largestNumber],
-										 y <- [x..largestNumber],
-										 isPalidrome (x * y)]
+					     y <- [x..largestNumber],
+						 isPalidrome (x * y)]
 
 	answer = maxPalindromProduct 100 999
 
