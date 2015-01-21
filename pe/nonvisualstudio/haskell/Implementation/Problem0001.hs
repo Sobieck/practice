b43193de33feae2dc92d278ff89d41b7828f76e3
Sobieck00 <thomas@sobieck.com>
2015-01-21 16:46:57 -0600

    How about this time.

--- pe/nonvisualstudio/haskell/Implementation/Problem0001.hs
+++ pe/nonvisualstudio/haskell/Implementation/Problem0001.hs
@@ -1,9 +1,9 @@
 module Problem0001 where
-	isMultiple number multipleOf = 0 == number `mod` multipleOf
+    isMultiple number multipleOf = 0 == number `mod` multipleOf
 
-	isMultipleOf3Or5 number | isMultiple number 3 	= True
-							| isMultiple number 5 	= True
-							| otherwise             = False
+    isMultipleOf3Or5 number | isMultiple number 3 	= True
+                            | isMultiple number 5 	= True
+                            | otherwise             = False
 
-	sumOfMultiplesOfThreeAndFive maxNumberOfListExclusive =
-		sum [x | x <- [1..(maxNumberOfListExclusive - 1)], isMultipleOf3Or5 x]
+    sumOfMultiplesOfThreeAndFive maxNumberOfListExclusive =
+        sum [x | x <- [1..(maxNumberOfListExclusive - 1)], isMultipleOf3Or5 x]
--- pe/nonvisualstudio/haskell/Implementation/Problem0004.hs
+++ pe/nonvisualstudio/haskell/Implementation/Problem0004.hs
@@ -1,22 +1,22 @@
 module Problem0004 where
 
-	zipNumberWithReverse number = zip (show number) (reverse (show number))
+    zipNumberWithReverse number = zip (show number) (reverse (show number))
 
-	lengthOfArrayThatAddsOneToOddNumbers array 	| odd (length array) 	= (length array) + 1
-												| otherwise				= length array
+    lengthOfArrayThatAddsOneToOddNumbers array 	| odd (length array) 	= (length array) + 1
+                                                | otherwise				= length array
 
-	halveArray array = take (n `div` 2) array
-		where n = lengthOfArrayThatAddsOneToOddNumbers array
+    halveArray array = take (n `div` 2) array
+        where n = lengthOfArrayThatAddsOneToOddNumbers array
 
-	doesTupleContainSameItem tuple = fst tuple == snd tuple
+    doesTupleContainSameItem tuple = fst tuple == snd tuple
 
-	isPalidrome number = and [doesTupleContainSameItem x | x <- halveArray (zipNumberWithReverse number)]
+    isPalidrome number = and [doesTupleContainSameItem x | x <- halveArray (zipNumberWithReverse number)]
 
-	maxPalindromProduct smallestNumber largestNumber =
-		maximum [x * y | x <- [smallestNumber..largestNumber],
-					     y <- [x..largestNumber],
-						 isPalidrome (x * y)]
+    maxPalindromProduct smallestNumber largestNumber =
+        maximum [x * y | x <- [smallestNumber..largestNumber],
+                         y <- [x..largestNumber],
+                         isPalidrome (x * y)]
 
-	answer = maxPalindromProduct 100 999
+    answer = maxPalindromProduct 100 999
 
-	largerNumberAnswer = maxPalindromProduct 1000 9999
+    largerNumberAnswer = maxPalindromProduct 1000 9999
