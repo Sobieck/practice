3234550a79f9ea9fc068a489795deee14dacc910
Sobieck00 <thomas@sobieck.com>
2015-08-01 20:45:11 -0500

    Finished another challenge

--- hr/nonvisualstudio/clojure/algorithms/implementation/Implementation/chocolate-feast.clj
+++ hr/nonvisualstudio/clojure/algorithms/implementation/Implementation/chocolate-feast.clj
@@ -8,3 +8,33 @@
   (int-division
     totalToSpend
     costPerChocolate))
+
+(defn number-of-freebies
+  [totalWrappers wrappersPerFreebie]
+  [(int-division
+    totalWrappers
+    wrappersPerFreebie)
+   (int
+     (mod
+       totalWrappers
+       wrappersPerFreebie))])
+
+(defn total-chocolates-consumed
+  [totalToSpend
+   costPerChocolate
+   wrappersPerFreebie]
+   (let [purchasedChocolates (number-to-purchase totalToSpend costPerChocolate)
+         freebies (first (number-of-freebies purchasedChocolates wrappersPerFreebie))
+         totalChocolates [purchasedChocolates freebies]
+         startRemainder (second (number-of-freebies purchasedChocolates wrappersPerFreebie))]
+         (loop
+           [chocs totalChocolates
+            remainder startRemainder]
+           (cond
+             (<
+               (+ remainder (last chocs)) wrappersPerFreebie)
+               (reduce + chocs)
+             :else
+              (recur
+                (conj chocs (first (number-of-freebies (+ (last chocs) remainder) wrappersPerFreebie)))
+                (second (number-of-freebies (+ (last chocs) remainder) wrappersPerFreebie)))))))
--- hr/nonvisualstudio/clojure/algorithms/implementation/Spec/chocolate-feast.Spec.clj
+++ hr/nonvisualstudio/clojure/algorithms/implementation/Spec/chocolate-feast.Spec.clj
@@ -11,10 +11,21 @@
     (is (= 0 (number-to-purchase 2 10)))
     (is (= 2 (number-to-purchase 10 4)))
     (is (= 666 (number-to-purchase 1000 1.5)))
-    (is (= 5291 (number-to-purchase 10000 1.89)))
+    (is (= 3 (number-to-purchase 6 2)))
+    (is (= 5291 (number-to-purchase 10000 1.89))))
+  (testing "number-of-freebies"
+    (is (= [0 1] (number-of-freebies 1 2)))
+    (is (= [100 0] (number-of-freebies 100 1)))
+    (is (= [2 0] (number-of-freebies 3 1.5)))
+    (is (= [1 1] (number-of-freebies 3 2))))
+  (testing "total-chocolates-consumed"
+    (is (= 6 (total-chocolates-consumed 10 2 5)))
+    (is (= 3 (total-chocolates-consumed 12 4 4)))
+    (is (= 5 (total-chocolates-consumed 6 2 2)))
+    (is (= 899 (total-chocolates-consumed 43203 60 5)))
 
-    )
+    ))
 
-    )
+
 
 (run-tests)
