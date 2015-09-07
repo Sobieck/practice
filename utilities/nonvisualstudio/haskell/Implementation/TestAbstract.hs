40594e092bb491c4d29b1c7fc106b80ce7084df1
Sobieck00 <thomas@sobieck.com>
2015-09-07 09:18:21 -0500

    Haskell HR

--- utilities/nonvisualstudio/haskell/Implementation/TestAbstract.hs
+++ utilities/nonvisualstudio/haskell/Implementation/TestAbstract.hs
@@ -9,3 +9,6 @@ module TestAbstract where
     easyAssertEqual functionName function input expected = assertEqual nameOfTest expected (function input)
         where
             nameOfTest = functionName ++ " " ++ (show input) ++ " should return " ++ (show expected) ++ "."
+
+    easyAssertEqualTwoInputs functionName function input1 input2 expected = assertEqual nameOfTest expected (function input1 input2)
+      where nameOfTest = functionName ++ " " ++ (show input1) ++ " " ++ (show input2) ++ " should return " ++ (show expected) ++ "."
