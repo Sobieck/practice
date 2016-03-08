8aecca49c14ee4db441b64f6a848dd539ca29e25
Thomas Sobieck <thomas@sobieck.com>
2016-03-08 16:28:28 -0600

    learning r.

--- coursera/programmingr/src/first.r
+++ coursera/programmingr/src/first.r
@@ -8,3 +8,37 @@ myfunction <- function() {
 second <- function(x) {
   x + rnorm(length(x))
 }
+
+avector <- function(){
+  1:100
+}
+
+boolvector <- function(){
+  c(T,F,T,F)
+}
+
+#coersion
+charcoersionvector <- function(){
+  c("a", TRUE)
+}
+
+explicitcoersion <- function(){
+  x <- 0:6
+  as.logical(x)
+}
+
+notcoersion <- function(){
+  x <- c("a", "b", "c")
+  as.numeric(x)
+}
+
+#matrices
+
+matrixone <- function(){
+  matrix(nrow = 2, ncol = 3)
+}
+
+matrixonedim <- function(){
+  matrix <- matrixone()
+  dim(matrix)
+}
