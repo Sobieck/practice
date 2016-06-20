c6425a138501acbdd5948e051367a13542070138
Sobieck00 <thomas@sobieck.com>
2016-06-20 08:36:14 -0500

    working.

--- pe/nonvisualstudio/haskell/Implementation/problem0030/Solution.hs
+++ pe/nonvisualstudio/haskell/Implementation/problem0030/Solution.hs
@@ -1,4 +1,20 @@
 module Problem0030 where
 
-  valueOfDigits :: Integer -> [Integer]
-  valueOfDigits number = [number]
+  import Data.Char
+  import Data.List(sort)
+
+  valuesOfDigits :: Int -> [Int]
+  valuesOfDigits number = valOfDigits (show number) []
+    where
+      valOfDigits :: String -> [Int] -> [Int]
+      valOfDigits "" result = reverse $ sort result
+      valOfDigits number result = valOfDigits (tail number) ((digitToInt (head number)) : result)
+
+
+-- max (exp + 1) * (9 ^ exp)
+--1. Figure out theoretical max
+--2. Splitter
+--3. Sieve. Falsify all that have a number and are less than the highest input.
+--    A. Sort by max number for each number. Maybe we need a tuple to keep track of original number.
+--           a. Haskell type. Original number, Current sum, array of each original number sorted, array of --powers
+--    B. Delete from array all that are false.
--- pe/nonvisualstudio/haskell/Implementation/problem0030/Tests.hs
+++ pe/nonvisualstudio/haskell/Implementation/problem0030/Tests.hs
@@ -2,14 +2,30 @@ module Problem0030Tests where
   import Test.Hspec
   import Test.QuickCheck
   import Control.Exception (evaluate)
+  import Data.List(sort)
 
   import Problem0030
 
   tests :: IO ()
   tests = hspec $ do
-  describe "Problem0030.valueOfDigits" $ do
+  describe "Problem0030.valuesOfDigits" $ do
     it "returns [0] when given 0" $ do
-      valueOfDigits 0 `shouldBe` ([0] :: [Integer])
+      valuesOfDigits 0 `shouldBe` ([0] :: [Int])
+
+    it "returns [1, 0] when given 10" $ do
+      valuesOfDigits 10 `shouldBe` ([1, 0] :: [Int])
+
+    it "returns [8, 6, 2, 1, 0] when given 16082" $ do
+      valuesOfDigits 16082 `shouldBe` ([8, 6, 2, 1, 0] :: [Int])
 
     it "returns a list of numbers with the same number of of elements as the length of the number" $
-      property $ \x -> (length $ valueOfDigits $ abs x) == (length $ show $ abs $ x :: Int)
+      property $ \x -> (length $ valuesOfDigits $ abs x) == (length $ show $ abs $ x :: Int)
+
+    it "returns a list of numbers that do not exceed the value of 9" $
+      property $ \x -> all (<=9) $ valuesOfDigits $ abs x
+
+    it "returns a list of numbers that all are greater than or equal to 0" $
+      property $ \x -> all (>=0) $ valuesOfDigits $ abs x
+
+    it "returns the values in descending order" $
+      property $ \x -> (valuesOfDigits $ abs x) == (reverse $ sort $ valuesOfDigits $ abs x)
