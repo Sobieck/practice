e2877983852a375527af524d4dbc19b8e0f8896d
Sobieck00 <thomas@sobieck.com>
2016-07-10 08:28:43 -0500

    Check in...

--- pe/nonvisualstudio/haskell/problem0031/Solution.hs
+++ pe/nonvisualstudio/haskell/problem0031/Solution.hs
@@ -2,7 +2,8 @@ module Problem0031 where
 
   coinSums :: Int -> [Int] -> Int
   coinSums amount coinDenominations = 5
-    where coinRecur currentDenomination maxCountOfCurrent denomLeft 
+    where coinRecur currentDenomination maxCountOfCurrent denomsLeft currentCount currentSum =
+
 
 -- array: 10, 5, 1
 -- current: 10
