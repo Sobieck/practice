8a44897e729e85d78fbe2ba4635d945f27d1c271
Sobieck00 <thomas@sobieck.com>
2016-07-11 11:39:11 -0500

    working with r

---
+++ kaggle/titanic/importData.r
@@ -0,0 +1,11 @@
+# source("titanic/importData.r")
+
+# Import the training set: train
+train_url <- "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+train <- read.csv(train_url)
+
+# Import the testing set: test
+test_url <- "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
+test <-  read.csv(test_url)
+
+#str(train)
