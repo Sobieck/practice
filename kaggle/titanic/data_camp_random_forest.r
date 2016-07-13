19c99a8eec718e8ca8e681883f3ba7a891a09ec9
Sobieck00 <thomas@sobieck.com>
2016-07-13 12:50:42 -0500

    more stuff

--- kaggle/titanic/data_camp_random_forest.r
+++ kaggle/titanic/data_camp_random_forest.r
@@ -16,13 +16,42 @@ test_to_clean$Survived <- NA
 
 all_data <- as.vector(rbind(train_to_clean, test_to_clean))
 
-#embarked has empty items. Assign "S" because it is common
-missing_embarked <- all_data$PassengerId[all_data$Embarked == ""]
-all_data$Embarked[missing_embarked] <- "S"
+extractTitles <- function(data_set){
 
-# Factorize embarkment codes.
-all_data$Embarked <- factor(all_data$Embarked)
+  vector_of_strings <- data_set$Name
+  name_char <- as.character(vector_of_strings)
+  data_set$title <- trimws(sapply(strsplit(name_char, split=','), function(x) (sapply(strsplit(x[2], split='.', fixed=TRUE), function(x) (x[1])))))
 
-#missing fare values -- IS NA
-missing_fare <- all_data[!complete.cases(all_data$Fare),]
-all_data$Fare[missing_fare$PassengerId] <- median(all_data$Fare, na.rm = TRUE)
+  data_set$title[data_set$title == "Dona"] <- "Miss"
+
+  return (data_set)
+}
+
+cleanData <- function(data_set){
+  #embarked has empty items. Assign "S" because it is common
+  missing_embarked <- data_set$PassengerId[data_set$Embarked == ""]
+  data_set$Embarked[missing_embarked] <- "S"
+
+  # Factorize embarkment codes.
+  data_set$Embarked <- factor(data_set$Embarked)
+
+  #missing fare values -- IS NA
+  missing_fare <- data_set[!complete.cases(data_set$Fare),]
+  data_set$Fare[missing_fare$PassengerId] <- median(data_set$Fare, na.rm = TRUE)
+
+  # add titles and family_size
+  data_set <- extractTitles(data_set)
+  data_set$family_size <- data_set$SibSp + data_set$Parch + 1
+
+
+  # How to fill in missing Age values?
+  # We make a prediction of a passengers Age using the other variables and a decision tree model.
+  # This time you give method = "anova" since you are predicting a continuous variable.
+  predicted_age <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + title + family_size,
+                         data = data_set[!is.na(data_set$Age),], method = "anova")
+  data_set$Age[is.na(data_set$Age)] <- predict(predicted_age, data_set[is.na(data_set$Age),])
+
+  return (data_set)
+}
+
+cleaned_data <- cleanData(all_data)
