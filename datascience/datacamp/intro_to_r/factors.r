de3bbd489e0cb42a4ba6bcb22a17b037c41427fa
Sobieck00 <thomas@sobieck.com>
2016-08-11 11:58:05 -0500

    factors.

--- datascience/datacamp/intro_to_r/factors.r
+++ datascience/datacamp/intro_to_r/factors.r
@@ -11,7 +11,20 @@ animals_vector <- c("Elephant", "Giraffe", "Donkey", "Horse")
 factor_animals_vector <- factor(animals_vector)
 factor_animals_vector
 
+low <- "Low"
+med <- "Medium"
+high <- "High"
+
 # Temperature - ordinal - order can be asserted
-temperature_vector <- c("High", "Low", "High","Low", "Medium")
-factor_temperature_vector <- factor(temperature_vector, order = TRUE, levels = c("Low", "Medium", "High"))
+temperature_vector <- c(high, low, high, low, med)
+factor_temperature_vector <- factor(temperature_vector, order = TRUE, levels = c(low, med, high))
 factor_temperature_vector
+
+# Code to build factor_survey_vector
+survey_vector <- c("M", "F", "F", "M", "M")
+factor_survey_vector <- factor(survey_vector)
+
+# Specify the levels of factor_survey_vector
+levels(factor_survey_vector) <- c(female, male)
+
+summary(factor_survey_vector)
