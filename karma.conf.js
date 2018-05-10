7276dd4c44caf06065db8df03eb3b46d8a4462c1
Thomas Sobieck <thomas@sobieck.com>
2018-05-09 19:58:18 -0500

    Making progress.

--- karma.conf.js
+++ karma.conf.js
@@ -1,76 +1,77 @@
-﻿//karma updaes
-//npm update karma-ng-html2js-preprocessor
-//npm update karma
-//npm update karma-ie-launcher
+// Karma configuration
+// Generated on Wed May 09 2018 19:07:16 GMT-0500 (DST)
 
-module.exports = function (config) {
-    config.set({
+module.exports = function(config) {
+  config.set({
 
-        // base path, that will be used to resolve files and exclude
-        basePath: '',
-
-        // frameworks to use
-        frameworks: ['jasmine'],
-
-        // list of files / patterns to load in the browser
-        files: [
-            'http://peterolson.github.com/BigInteger.js/BigInteger.min.js',
-            { pattern: 'utilities/**/*.js', include: true },
-            { pattern: 'algorithms/visualstudio/algorithms.javascript/**/*.js', include: true },
-            { pattern: 'pe/visualstudio/PE.JavaScript/**/*.js', include: true },
-            { pattern: 'pe/problemsets/js/*.js'}
-        ],
-
-        // list of files to exclude
-        exclude: [
-        ],
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
 
-        // Testing Directives
-        preprocessors: {
-        },
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters: ['dots'],
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
 
-        // web server port
-        port: 9876,
-
-        //html
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+    // list of files / patterns to load in the browser
+    files: [
+      'http://peterolson.github.com/BigInteger.js/BigInteger.min.js',
+      '**/*.js',
+      '**/*Spec.js'
+    ],
 
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
-
-        reportSlowerThan: 100,
-
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera (has to be installed with `npm install karma-opera-launcher`)
-        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-        // - PhantomJS
-        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-        browsers: ['Firefox', 'Chrome'],
-        // , 'IE'
+    // list of files / patterns to exclude
+    exclude: [
+      '**/node_modules/**'
+    ],
 
 
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+    plugins : [
+      "karma-jasmine",
+      "karma-chrome-launcher"
+    ],
 
 
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: false
-    });
-};
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['ChromeHeadless'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
